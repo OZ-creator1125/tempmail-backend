887f40f8b9be56583b095df0bc2ddcadd5dc19a8
OZ-creator1125 <coz_dita@hotmail.com>
2026-02-26 15:58:35 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend funcionando correctamente 🚀" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor activo en puerto", PORT);
+});
